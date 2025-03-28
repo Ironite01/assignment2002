7c7ad0c17a1e80e063b1a8281e2ed8d220b86718
Juxwt <73164010+Juxwt@users.noreply.github.com>
2025-03-28 16:24:41 +0800

    Added Manager

---
+++ Manager.java
@@ -0,0 +1,39 @@
+public class Manager {
+    public void btoProjectListings(int mode){ //1 - Create, 2 - Edit, 3 - Delete
+        
+    }
+
+    public void toggleProjectVisiblity(){
+
+    }
+
+    public void viewOtherProjects(){ //Filterable Function
+        
+    }
+
+    public void manageOfficerRegis(){
+
+    }
+
+    public void manageBTOApp(){
+
+    }
+
+    public void manageBTOWithdraw(){
+
+    }
+
+    public void generateReport(){ //Filterable
+
+    }
+
+    public void viewAllEnq(){
+
+    }
+
+    //ADD 2 Methods Here
+    // View & Reply Enquires
+    // View Project Details
+
+
+}
